f49346f993f848e3277e60ea93fe1fab6963658d
juholee <juholee1010@gmail.com>
2021-06-25 20:45:47 -0400

    First commit

---
+++ memories/client/src/components/PostDetails/PostDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PostDetails = () => {
+    console.log("Post Details");
+    return ( 
+        <div>
+            Post details
+        </div>
+    )
+}
+
+export default PostDetails
